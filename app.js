4c3364701bac4e538d6d3331b6789753a1440bc4
Lenny <riel_lenny@yahoo.co.uk>
2020-10-28 17:03:56 +0200

    adding articles view/partials

--- app.js
+++ app.js
@@ -15,13 +15,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    const articles = [{
-        title: "test title",
-        createAt: Date.now(),
-        description: "test description"
-    }];
+    const articles = [
+        {
+            title: "Test Title",
+            createdAt: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae odit suscipit dolor, dolore commodi maxime explicabo necessitatibus, consequuntur labore delectus perspiciatis similique recusandae totam quasi aliquam nam, soluta dolorem."
+        },
+        {
+            title: "Test Title 2",
+            createdAt: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae odit suscipit dolor, dolore commodi maxime explicabo necessitatibus, consequuntur labore delectus perspiciatis similique recusandae totam quasi aliquam nam, soluta dolorem."
+        },
+];
 
-    res.render('index', {articles: articles});
+    res.render('articles/index', {articles: articles});
 });
 
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.send('articles page')
 });
 
+router.get('/new', (req, res) => {
+    res.render('articles/new')
+});
+
+router.post('/', (req, res) => {
+    
+});
+
 module.exports = router;
