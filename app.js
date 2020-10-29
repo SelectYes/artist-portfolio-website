b2d1117d778f1536cdbcab55ce988bdf77430dcb
Lenny <riel_lenny@yahoo.co.uk>
2020-10-29 17:33:32 +0200

    added article routes/new article form

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 3000;
+
+//IMPORT MODELS
+const Article = require('./models/article');
+
+
+// MONGOOSE CONFIG 
+mongoose.connect('mongodb://localhost:27017/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to DB!"))
+.catch(error => console.log(error.message))
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
 
 // ROUTES
 const articlesRouter = require('./routes/articles');
 
-// ROUTES CONFIIG
-app.use('/articles', articlesRouter);
 
 // CSS CONFIG
 app.use(express.static(__dirname + '/public'));
 
-//EJS CONFIG
-app.set('view engine', 'ejs');
 
+
+//INDEX
 app.get('/', (req, res) => {
     const articles = [
         {
@@ -26,9 +42,12 @@ app.get('/', (req, res) => {
             createdAt: new Date(),
             description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae odit suscipit dolor, dolore commodi maxime explicabo necessitatibus, consequuntur labore delectus perspiciatis similique recusandae totam quasi aliquam nam, soluta dolorem."
         },
-];
+    ];
 
     res.render('articles/index', {articles: articles});
 });
+
+// ROUTES CONFIG
+app.use('/articles', articlesRouter);
 
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
--- routes/articles.js
+++ routes/articles.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const Article = require('../models/article');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('articles page')
-});
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', { article: new Article() })
 });
 
-router.post('/', (req, res) => {
-    
+router.get('/:id', async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.id);
+        res.render('articles/show', {article: article})
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    });
+
+    try {
+        await article.save();
+        res.redirect(`/articles/${article._id}`);
+    } catch (error) {
+        // console.log(error)
+        res.render('articles/new', { article: article })
+    }
 });
 
 module.exports = router;
